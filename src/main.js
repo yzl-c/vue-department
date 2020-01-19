11eddc5444322ad72648cd61ab04e91f770b5f19
yzl-c <1120779716@qq.com>
2020-01-19 19:53:57 +0800

    登录界面

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+// 全局样式css
+import './assets/css/global.css'
+// 导入axios
+import axios from 'axios'
+// 配置请求根路径
+axios.defaults.baseURL = 'http://localhost:8081'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
